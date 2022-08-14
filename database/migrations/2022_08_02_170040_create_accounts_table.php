4e5d37af068020e3ba4c5f24b6a52a61dfb3335a
Elliot Derhay <elliot@elliotderhay.com>
2022-08-14 08:14:33 -0400

    Add foreign key constraint for remote source

--- database/migrations/2022_08_02_170040_create_accounts_table.php
+++ database/migrations/2022_08_02_170040_create_accounts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('remote_source_id');
+            $table->foreignId('remote_source_id')->constrained();
             $table->string('name');
             $table->string('slug');
             $table->string('shorthand');
