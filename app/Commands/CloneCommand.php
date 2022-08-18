5e1db7a3a8d36902bd49c25c4f2006cb4622d1d3
Elliot Derhay <elliot@elliotderhay.com>
2022-08-18 13:39:59 -0400

    Get pattern down and add debug option

--- app/Commands/CloneCommand.php
+++ app/Commands/CloneCommand.php
@@ -7,12 +7,16 @@ use LaravelZero\Framework\Commands\Command;
 
 class CloneCommand extends Command
 {
+    protected string $pattern = "/^(.*)[\/:](.*)\/(.*)$/i";
+
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'clone {url}';
+    protected $signature = 'clone
+        {url : The URL pointing to the repository to clone.}
+        {--d|debug : Log the input, regexp pattern, and output of `preg_match`.}';
 
     /**
      * The description of the command.
@@ -24,12 +28,52 @@ class CloneCommand extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return int
      */
-    public function handle()
+    public function handle(): int
     {
-        $this->info("Cloning '" . $this->argument('url') . "'...");
+        try {
+            [$url, $host, $account, $repo] = $this->matches();
+        } catch (\Throwable) {
+            return self::FAILURE;
+        }
 
         return self::SUCCESS;
+    }
+
+    protected function matches(): array
+    {
+        // `preg_match()`, Y U NO TAKE ARRAY VAR FOR `$matches`?? (╯°□°)╯︵ ┻━┻
+        $matches = null;
+
+        if($this->option('debug')) {
+            $this->info("Running in debug mode!\n");
+            $this->line("Received input: {$this->argument('url')}");
+            $this->line("Pattern: {$this->pattern}\n");
+        };
+
+        /**
+         * Pattern doesn't include 'git@' or 'https://' prefix because this is intended to work with shorthands as well.
+         */
+        if(!preg_match($this->pattern, $this->argument('url'), $matches)) {
+            $this->error("URL string does not match expected pattern.");
+            $this->error("Pattern: \<host\>(':' or '/')\<account\>/\<repository\>");
+
+            // Force throwing an error, stopping execution in `try`
+            return [];
+        }
+
+        if($this->option('debug')) {
+            $this->info("Matches found:");
+            $this->table(
+                ['url', 'host', 'account', 'repo'],
+                [$matches]
+            );
+
+            // Force throwing an error, stopping execution in `try`
+            return [];
+        }
+
+        return $matches;
     }
 }
